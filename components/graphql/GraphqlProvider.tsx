16f963699203a43f061282f3d5ceb94691b44128
mubin <mubin.ice.ru@gmail.com>
2024-04-06 14:54:59 +0600

    update api endpoint

--- components/graphql/GraphqlProvider.tsx
+++ components/graphql/GraphqlProvider.tsx
@@ -8,7 +8,7 @@ const prod_url = "http://35.208.204.126/api/graphql/"
 const local_url = "http://localhost:3001/graphql"
 
 const link = createUploadLink({
-  uri: local_url,
+  uri: prod_url,
 })
 
 const client = new ApolloClient({
