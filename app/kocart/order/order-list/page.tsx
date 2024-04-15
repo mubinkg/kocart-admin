4be857bb9850bb10628d833adec82709cd759120
mubin <mubin.ice.ru@gmail.com>
2024-04-15 23:02:57 +0600

    order bar

--- app/kocart/order/order-list/page.tsx
+++ app/kocart/order/order-list/page.tsx
@@ -1,7 +1,15 @@
+import { BreadCrumb } from "primereact/breadcrumb";
+import { Card } from "primereact/card";
+
 export default function OrderList(){
+    const items = [
+        { label: 'Order' },
+        { label: 'Order List' }
+    ];
     return (
         <div>
-            Order List
+            <BreadCrumb model={items} className="m-4"/>
+            <Card className="m-4"></Card>
         </div>
     )
 }
