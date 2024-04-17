8860bbbe349328f4e8db6adce276a3302e7de997
mubin <mubin.ice.ru@gmail.com>
2024-04-17 22:30:17 +0600

    integrate order list api

--- app/kocart/order/order-list/page.tsx
+++ app/kocart/order/order-list/page.tsx
@@ -1,15 +1,75 @@
+'use client'
+
+import { GET_ORDER_LIST } from "@/graphql/order";
+import { useQuery } from "@apollo/client";
 import { BreadCrumb } from "primereact/breadcrumb";
 import { Card } from "primereact/card";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { Toast } from "primereact/toast";
+import { useEffect, useRef } from "react";
 
-export default function OrderList(){
+export default function OrderList() {
+    const toast = useRef<any>(null)
+
+    const showError = (message: string) => {
+        toast.current.show({ severity: 'error', summary: 'Error', detail: message, life: 3000 });
+    }
+    const { data, error } = useQuery(GET_ORDER_LIST, {
+        variables: {
+            "adminOrderListInput": {
+                "end_date": null,
+                "payment_method": null,
+                "product_type": null,
+                "start_date": null,
+                "status": null
+            }
+        }
+    })
+    useEffect(() => {
+        if (error) {
+            showError('Error on fetching order list.')
+        }
+    }, [error])
     const items = [
         { label: 'Order' },
         { label: 'Order List' }
     ];
+
+    const orderAction = () => (
+        <div className="flex flex-column">
+            <i className="pi pi-eye mb-3" />
+            <i className="pi pi-trash mb-3"/>
+            <i className="pi pi-file mb-3"/>
+            <i className="pi pi-phone mb-3"/>
+            <i className="pi pi-map-marker"/>
+        </div>
+    )
+
     return (
         <div>
-            <BreadCrumb model={items} className="m-4"/>
-            <Card className="m-4"></Card>
+            <BreadCrumb model={items} className="m-4" />
+            <Toast ref={toast} />
+            <Card className="m-4">
+                <DataTable
+                    lazy
+                    totalRecords={data?.getAdminOrderList?.count ? data?.getAdminOrderList?.count : 0}
+                    value={data?.getAdminOrderList?.orders ? data?.getAdminOrderList?.orders : []}
+                >
+                    <Column field="_id" header="Order ID"></Column>
+                    <Column field="user.name" header="User Name"></Column>
+                    <Column field="order" header="Sellers"></Column>
+                    <Column field="order_note" header="O. Notes"></Column>
+                    <Column field="total" header="Total" />
+                    <Column field="delivery_charge" header="D.Charge" />
+                    <Column field="wallet_balance_used" header="Wallet Used($)" />
+                    <Column field="promo_discount" header="Promo disc.($)" />
+                    <Column field="final_total" header="Final Total($)" />
+                    <Column field="payment_method" header="Payment Method" />
+                    <Column field="created_at" header="Order Date" />
+                    <Column body={() => orderAction()} header="Action" />
+                </DataTable>
+            </Card>
         </div>
     )
 }
---
+++ graphql/order/index.ts
@@ -0,0 +1,35 @@
+import { gql } from "@apollo/client";
+
+export const GET_ORDER_LIST = gql`query GetAdminOrderList($adminOrderListInput: GetAdminOrderInput!) {
+    getAdminOrderList(adminOrderListInput: $adminOrderListInput) {
+      count
+      orders {
+        _id
+        user {
+          _id
+          name
+        }
+        product_variants {
+          product {
+            seller {
+              name
+              _id
+            }
+            _id
+          }
+          _id
+        }
+        order_note
+        total
+        delivery_charge
+        wallet_balance_used
+        promo_discount
+        promo_code
+        final_total
+        mobile
+        is_wallet_used
+        payment_method
+        created_at
+      }
+    }
+  }`
