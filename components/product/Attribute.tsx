14fc893046c942a95608b9ca3642e3a4b6e416b5
mubin <mubin.ice.ru@gmail.com>
2024-05-08 23:10:41 +0600

    udpate ad attribute

--- components/product/Attribute.tsx
+++ components/product/Attribute.tsx
@@ -3,14 +3,14 @@
 import { Button } from 'primereact/button'
 import { ColorPicker } from 'primereact/colorpicker';
 import { Dropdown } from 'primereact/dropdown';
+import { FileUpload } from 'primereact/fileupload';
 import { InputText } from 'primereact/inputtext';
-import React, { useState } from 'react'
-import { useFieldArray, useForm } from 'react-hook-form'
+import React from 'react'
+import { useFieldArray, useForm, Controller } from 'react-hook-form'
 import { v4 as uuidv4 } from 'uuid';
 
-
-const Attribute = ({handleAddAttribute}:any) => {
-    const { control, register, handleSubmit , watch, setValue, reset} = useForm()
+const Attribute = ({ handleAddAttribute }: any) => {
+    const { control, register, handleSubmit, watch, setValue, reset } = useForm()
     const { fields, append, remove } = useFieldArray<any>({
         control,
         name: 'attribute'
@@ -20,15 +20,15 @@ const Attribute = ({handleAddAttribute}:any) => {
 
     const submitHandler = (value: any) => {
         let values = []
-        if(value?.data){
-            values = value.data.map((v:any)=>({
+        if (value?.data) {
+            values = value.data.map((v: any) => ({
                 valueName: v?.attributeName || "",
                 image: v?.image || null,
                 color: v?.color || null,
                 type: v?.attributeType || 'DEFAULT'
             }))
             handleAddAttribute(values, reset)
-        }else{
+        } else {
 
         }
     }
@@ -37,47 +37,65 @@ const Attribute = ({handleAddAttribute}:any) => {
         <div className="flex flex-column gap-4">
             <div className="flex mt-4 gap-4">
                 <p className="mb-2 font-semibold">Attribute Values<span style={{ color: 'red' }}>*</span></p>
-                <Button label="Attribute Value" onClick={append} />
+                <Button label="Attribute Value" onClick={()=>append({attributeName:"",attributeType:cities[0],image: "",color:""})} />
             </div>
             <div className='flex flex-column gap-5'>
                 {fields.map((field, index) => (
                     <div key={uuidv4()} className='flex gap-5'>
                         <div className='flex flex-column w-5'>
                             <label className='mb-2'>Attribute Name</label>
-                            <InputText
-                                key={uuidv4()} // important to include key with field's id
-                                {...register(`data.${index}.attributeName`)}
+                            <Controller
+                                name={`attribute.${index}.attributeName`}
+                                control={control}
+                                render={({ field }) => (
+                                    <InputText
+                                        {...field}
+                                    />
+                                )}
                             />
                         </div>
                         <div className='flex flex-column w-5'>
                             <label className='mb-2'>Attribute Type</label>
-                            <Dropdown 
-                                key={uuidv4()}
-                                value={watch(`data.${index}.attributeType`) || "DEFAULT"}
-                                onChange={(e)=>setValue(`data.${index}.attributeType`,e.target.value)}
-                                options={cities}
+                            <Controller
+                                control={control}
+                                name={`attribute.${index}.attributeType`}
+                                render={({ field }) => (
+                                    <Dropdown
+                                        {...field}
+                                        defaultValue={cities[0]}
+                                        options={cities}
+                                    />
+                                )}
                             />
                             {
-                                watch(`data.${index}.attributeType`) === 'IMAGE'? 
-                                <input
-                                    className='mt-2' 
-                                    type='file'
-                                    {...register(`data.${index}.image`)}
-                                />
-                                : ""
+                                watch(`attribute.${index}.attributeType`) === 'IMAGE' ?
+                                    <Controller
+                                        name={`attribute.${index}.image`}
+                                        control={control}
+                                        render={({field})=>(
+                                            <FileUpload {...field}/>
+                                        )}
+                                    />
+                                    : ""
                             }
                             {
-                                (watch(`data.${index}.attributeType`)) === 'COLOR'? 
-                                <ColorPicker 
-                                    className='mt-2'
-                                    format='hex' 
-                                    value={watch(`data.${index}.color`)}
-                                    onChange={(e)=>setValue(`data.${index}.color`, e.target.value)}
-                                />
-                                :""
+                                (watch(`attribute.${index}.attributeType`)) === 'COLOR' ?
+                                    <Controller
+                                        control={control}
+                                        name={`attribute.${index}.color`}
+                                        render={({ field }) => (
+                                            <ColorPicker
+                                                className='mt-2'
+                                                format='hex'
+                                                {...field}
+                                            />
+                                        )}
+                                    />
+
+                                    : ""
                             }
                         </div>
-                        <Button onClick={()=>remove()} icon='pi pi-times' className='mt-4' style={{height: "50px"}}/>
+                        <Button onClick={() => remove(index)} icon='pi pi-times' className='mt-4' style={{ height: "50px" }} />
                     </div>
                 ))}
             </div>
