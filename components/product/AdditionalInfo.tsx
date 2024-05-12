3c8cd2cce0950584d3b25a4047ff016f7ef4e31b
mubin <mubin.ice.ru@gmail.com>
2024-05-13 04:44:29 +0600

    update digital product host info

--- components/product/AdditionalInfo.tsx
+++ components/product/AdditionalInfo.tsx
@@ -2,15 +2,31 @@
 import { stockStatusOptions, stockTypeOptions, typeOfDgitalProductOptions, typeOfProductOptions } from '@/data/product/items'
 import { Button } from 'primereact/button'
 import { Checkbox } from 'primereact/checkbox'
+import { FileUpload } from 'primereact/fileupload'
 import { InputNumber } from 'primereact/inputnumber'
 import { InputSwitch } from 'primereact/inputswitch'
 import { InputText } from 'primereact/inputtext'
-import { useForm } from 'react-hook-form'
+import { useForm, Controller } from 'react-hook-form'
 import Select from 'react-select'
 
+const downloadLinkTypeOptions = [
+    {
+        label: 'None',
+        value: "none"
+    },
+    {
+        label: "Self Hosted",
+        value: 'self_hosted'
+    },
+    {
+        label: 'Add Link',
+        value: 'add_link'
+    }
+]
+
 export default function AdditionalInfo({ productType }: { productType: any }) {
-    const { setValue, watch, handleSubmit } = useForm()
-    function submitHandler(values:any){
+    const { setValue, watch, handleSubmit, register, control } = useForm()
+    function submitHandler(values: any) {
         console.log(values)
     }
 
@@ -71,6 +87,48 @@ export default function AdditionalInfo({ productType }: { productType: any }) {
                     <div className="flex align-items-center gap-3 mt-3">
                         <Checkbox checked={watch('stock_management')} onChange={(e) => setValue('stock_management', e.checked ? true : false)} />
                         <p>Enable Stock Management</p>
+                    </div>
+                ) : ""
+            }
+            {
+                watch('type_of_product') === 'digital' ? (
+                    <div className="flex align-items-start gap-4 mt-3">
+                        <div>
+                            <p>Is Download Allowed?</p>
+                            <Checkbox checked={watch('download_allowed')} onChange={(e) => setValue('download_allowed', e.checked ? true : false)} />
+                        </div>
+                        {
+                            watch('download_allowed') === true ?
+                                (
+                                    <div className='w-3'>
+                                        <p>Download Link Type *</p>
+                                        <Select
+                                            name='download_link_type'
+                                            options={downloadLinkTypeOptions}
+                                            defaultValue={downloadLinkTypeOptions[0]}
+                                            onChange={(e)=>setValue('download_link_type',e?.value)}
+                                        />
+
+                                    </div>
+                                )
+                                : ""
+                        }
+                        {
+                            watch('download_link_type') === 'self_hosted' ? (
+                                <div>
+                                    <p>File</p>
+                                    <FileUpload/>
+                                </div>) : ""
+                        }
+                        {
+                            watch('download_link_type') === 'add_link' ? 
+                            (
+                                <div>
+                                    <p>Digital Product Link *</p>
+                                    <InputText style={{height: "40px"}}/>
+                                </div>
+                            ) : ""
+                        }
                     </div>
                 ) : ""
             }
