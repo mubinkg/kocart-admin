2d1932910d607c813f015e5dccd86b9ffa7b77e4
mubin <mubin.ice.ru@gmail.com>
2024-05-14 15:55:16 +0600

    add option for delete media

--- app/kocart/media/page.tsx
+++ app/kocart/media/page.tsx
@@ -1,14 +1,18 @@
 'use client'
 
 import FileDng from "@/components/file/FileDng";
-import { CREATE_MEDIA } from "@/graphql/media/indes";
-import { useMutation } from "@apollo/client";
+import { CREATE_MEDIA, DELETE_MEDIA, GET_MEDIA } from "@/graphql/media/indes";
+import { useMutation, useQuery } from "@apollo/client";
 import { extname } from "path";
 import { BreadCrumb } from "primereact/breadcrumb";
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { Image } from "primereact/image";
 import { Toast } from "primereact/toast";
 import { useEffect, useRef, useState } from "react";
+import Swal from "sweetalert2";
 
 
 const items = [
@@ -17,6 +21,8 @@ const items = [
 ];
 
 export default function Page(){
+    const {data:mediaList, refetch} = useQuery(GET_MEDIA)
+    const [deleteMedia, {data:deleteMediaData}] = useMutation(DELETE_MEDIA)
     const fileRef = useRef<any>(null)
     const toast = useRef<any>(null)
     const [clear, setClear] = useState(false)
@@ -36,7 +42,15 @@ export default function Page(){
             fileRef.current.value = null
             setClear((old)=>!old)
         }
-    }, [data, error])
+        if(deleteMediaData){
+            show({
+                message: 'Media deleted successfully.',
+                icon: 'info',
+                label:"Info"
+            })
+            refetch()
+        }
+    }, [data, error, deleteMediaData])
     
     function createMediaHandler(){
         if(fileRef?.current?.getFiles()?.length){
@@ -64,6 +78,37 @@ export default function Page(){
         }
     }
 
+    function deleteMediaHandler(id:any){
+        Swal.fire({
+            title:'Media',
+            text: 'Are you sure to delete this media content?',
+            icon: "warning"
+        }).then((res)=>{
+            if(res.isConfirmed){
+                deleteMedia({
+                    variables: {
+                        "removeMediaId": id
+                    }
+                })
+            }
+        }).catch(()=>{
+            Swal.fire({
+                title: "Media",
+                text: 'Error on deleting media content',
+                icon: "error"
+            })
+        })
+    }
+
+    const brandImageRenderer = (item: any) => <Image src={item?.file} alt="Brand Image" width="200"/>
+    const mediaSizeRenderer = (item:any)=><p>{item.size/1000} kb</p>
+    const mediaNameRenderer = (item:any)=><p style={{maxWidth: "150px", wordWrap: "break-word"}}>{item.name}</p>
+    const mediaActionRenderer = (item:any)=>(
+        <div>
+            <i className="pi pi-trash cursor-pointer" onClick={()=>deleteMediaHandler(item?._id)}/>
+        </div>
+    )
+
     return(
         <div>
             <BreadCrumb model={items} className="m-4" />
@@ -71,6 +116,23 @@ export default function Page(){
             <Card className="m-4">  
                 <FileDng fileUploadRef={fileRef} clear={clear}/>
                 <Button onClick={createMediaHandler} label={loading?"Loading...": "Upload"} className="mt-4"/>
+                <DataTable 
+                    totalRecords={mediaList?.adminMedia?.count?mediaList?.adminMedia?.count:0}
+                    value={mediaList?.adminMedia?.media?mediaList?.adminMedia?.media:[]}
+                    lazy
+                    paginator 
+                    rows={1000} 
+                    rowsPerPageOptions={[1000, 2000, 30000]}
+                    className="mt-4"
+                >
+                    <Column field="_id" header="ID"/>
+                    <Column body={mediaNameRenderer} header="Name"/>
+                    <Column body={brandImageRenderer} header="Image"/>
+                    <Column field="extension" header="Extension"/>
+                    <Column field="subDirectory" header="Subdirectory"/>
+                    <Column body={mediaSizeRenderer} header="Size"/>
+                    <Column body={mediaActionRenderer} header="Action"/>
+                </DataTable>
             </Card>
         </div>
     )
--- graphql/media/indes.ts
+++ graphql/media/indes.ts
@@ -5,3 +5,24 @@ export const CREATE_MEDIA = gql`mutation CreateMedia($createMediaInput: CreateMe
       _id
     }
   }`
+
+export const GET_MEDIA = gql`query AdminMedia {
+  adminMedia {
+    count
+    media {
+      _id
+      extension
+      file
+      name
+      size
+      subDirectory
+      type
+    }
+  }
+}`
+
+export const DELETE_MEDIA = gql`mutation RemoveMedia($removeMediaId: String!) {
+  removeMedia(id: $removeMediaId) {
+    _id
+  }
+}`
