bb2067afa13313dcaf619fb7a957f0a0c49b51b6
mubin <mubin.ice.ru@gmail.com>
2024-05-18 00:46:22 +0600

    update url

--- components/graphql/GraphqlProvider.tsx
+++ components/graphql/GraphqlProvider.tsx
@@ -9,7 +9,7 @@ const prod_url = "http://35.208.204.126/api/graphql/"
 const local_url = "http://localhost:3001/graphql"
 
 const link = createUploadLink({
-  uri: prod_url,
+  uri: local_url,
 })
 
 const authLink = setContext((_, { headers }) => {
