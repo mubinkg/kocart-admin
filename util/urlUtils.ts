48b82e43a704a517834ee5b94660db9619341fbe
mubin <mubin.ice.ru@gmail.com>
2024-05-27 22:27:02 +0600

    update api base

--- util/urlUtils.ts
+++ util/urlUtils.ts
@@ -1,5 +1,5 @@
-// const env:any = 'production'
-const env = 'dev'
+const env:any = 'production'
+// const env = 'dev'
 export const loginApiUrl = env === 'dev' ?"http://localhost:3001/graphql" : "http://35.208.204.126/api/graphql/";
 export const loginUrl = env === 'dev' ? "http://localhost:3000/api/auth/" : "http://35.208.204.126/api/auth/";
 export const baseUrl = env === 'dev' ? "http://localhost:3001/graphql" : "http://35.208.204.126/api/graphql/" 
