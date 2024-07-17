c673158e25d210887a1a92742de62eb702dee4ec
mubin <mubin.ice.ru@gmail.com>
2024-07-17 13:03:04 +0600

    upate env

--- util/urlUtils.ts
+++ util/urlUtils.ts
@@ -1,5 +1,5 @@
-// const env:any = 'production'
-const env = 'dev'
+const env:any = 'production'
+// const env = 'dev'
 export const loginApiUrl = env === 'dev' ?"http://localhost:3001/graphql" : "http://kocart.com/api/graphql/";
 export const loginUrl = env === 'dev' ? "http://localhost:3000/api/auth/" : "http://kocart.com/api/auth/";
 export const baseUrl = env === 'dev' ? "http://localhost:3001/graphql" : "http://kocart.com/api/graphql/" 
