3d6b92405ef16ffe14e15541266404ff77a91fe8
mubin <mubin.ice.ru@gmail.com>
2024-07-17 11:01:33 +0600

    udpate endpoint

--- util/urlUtils.ts
+++ util/urlUtils.ts
@@ -1,5 +1,5 @@
 const env:any = 'production'
 // const env = 'dev'
-export const loginApiUrl = env === 'dev' ?"http://localhost:3001/graphql" : "http://35.208.204.126/api/graphql/";
-export const loginUrl = env === 'dev' ? "http://localhost:3000/api/auth/" : "http://35.208.204.126/api/auth/";
-export const baseUrl = env === 'dev' ? "http://localhost:3001/graphql" : "http://35.208.204.126/api/graphql/" 
+export const loginApiUrl = env === 'dev' ?"http://localhost:3001/graphql" : "http://kocart.com/api/graphql/";
+export const loginUrl = env === 'dev' ? "http://localhost:3000/api/auth/" : "http://kocart.com/api/auth/";
+export const baseUrl = env === 'dev' ? "http://localhost:3001/graphql" : "http://kocart.com/api/graphql/" 
