cfc829609d95586169b9d9143e19bf0e74747fc7
mubin <mubin.ice.ru@gmail.com>
2024-07-19 02:35:36 +0600

    add pagination on faq list

--- app/kocart/product/faq/page.tsx
+++ app/kocart/product/faq/page.tsx
@@ -1,28 +1,28 @@
 'use client'
 
 import { SLIDER_PRODUCT } from "@/graphql/slider";
-import { useLazyQuery, useMutation, useQuery } from "@apollo/client";
+import { useLazyQuery, useMutation } from "@apollo/client";
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { Dialog } from "primereact/dialog";
 import { InputText } from "primereact/inputtext";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import AsyncSelect from 'react-select/async'
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { Image } from "primereact/image";
 import { ADMIN_FAQ_LIST, CREATE_ADMIN_FAQ } from "@/graphql/faq";
 import { BreadCrumb } from "primereact/breadcrumb";
 
 
 export default function Page() {
-    const { data: faqList, refetch } = useQuery(ADMIN_FAQ_LIST, { variables: { limit: 1000, offset: 0 } })
+    const [getFaqList,{ data: faqList, refetch, loading:faqListLoading }] = useLazyQuery(ADMIN_FAQ_LIST, { fetchPolicy:"no-cache" })
     const [getSliderProduct] = useLazyQuery(SLIDER_PRODUCT)
     const [createAdminFaq, {loading}] = useMutation(CREATE_ADMIN_FAQ)
     const [visible, setVisible] = useState(false)
     const [product, setProduct] = useState('')
     const [question, setQuestion] = useState('')
     const [ans, setAns] = useState('')
+    const [pageData, setPageData] = useState<any>({rows:5,first:0})
 
     const loadProductOptions: any = async (val: string, callback: any) => {
         const data = await getSliderProduct({ variables: { query: val } })
@@ -49,6 +49,10 @@ export default function Page() {
 
     }
 
+    useEffect(()=>{
+        getFaqList({variables: { limit: pageData.rows, offset: pageData.first }})
+    },[,pageData])
+
     const items = [
         { label: 'Product' },
         { label: 'Product Faq List' }
@@ -59,7 +63,16 @@ export default function Page() {
             <BreadCrumb model={items} className='m-4' />
             <Card className="m-4 p-2">
                 <Button className="mb-4" label="Add New" onClick={() => setVisible(true)} />
-                <DataTable lazy totalRecords={faqList?.adminFaqList?.count ? faqList?.adminFaqList?.count : 0} onPage={(value) => console.log(value)} value={faqList?.adminFaqList?.faqs ? faqList?.adminFaqList?.faqs : []} paginator rows={1000} rowsPerPageOptions={[1000, 2000, 2500, 5000]}>
+                <DataTable 
+                    lazy 
+                    totalRecords={faqList?.adminFaqList?.count ? faqList?.adminFaqList?.count : 0} 
+                    onPage={(value) => setPageData(value)} 
+                    value={faqList?.adminFaqList?.faqs ? faqList?.adminFaqList?.faqs : []} 
+                    paginator 
+                    rows={pageData?.rows || 5}
+                    first={pageData?.first || 1} 
+                    rowsPerPageOptions={[5, 10, 25, 50]}
+                >
                     <Column field="_id" header="ID"></Column>
                     <Column field="question" header="Question"></Column>
                     <Column field="ans" header="Answr"></Column>
