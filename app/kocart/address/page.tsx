29b54604c2255596581852586f1992a1630d5bef
mubin <mubin.ice.ru@gmail.com>
2024-07-19 00:42:41 +0600

    add pagination at address list

--- app/kocart/address/page.tsx
+++ app/kocart/address/page.tsx
@@ -1,26 +1,44 @@
 'use client'
 
-import { useQuery } from "@apollo/client";
+import { useLazyQuery, useQuery } from "@apollo/client";
 import { Card } from "primereact/card";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { BreadCrumb } from "primereact/breadcrumb";
 import { ADMIN_CUSTOMER_LIST } from "@/graphql/customres";
+import { useEffect, useState } from "react";
 
 
 export default function Page() {
-    const { data: customerData, refetch } = useQuery(ADMIN_CUSTOMER_LIST, { variables: { limit: 1000, offset: 0, query: "" } })
+    const [getCustomers, {data: customerData,loading} ] = useLazyQuery(ADMIN_CUSTOMER_LIST, {fetchPolicy: "no-cache"})
+    const [pageData, setPageData] = useState<any>({rows:5,first:0})
 
     const items = [
         { label: 'Customer' },
         { label: 'Address List' }
     ];
 
+    useEffect(()=>{
+        getCustomers({variables:{ limit: pageData.rows, offset: pageData.first, query: "" }})
+    }, [])
+
+    useEffect(()=>{
+        getCustomers({variables:{ limit: pageData.rows, offset: pageData.first, query: "" }})
+    }, [pageData])
+
     return (
         <div>
             <BreadCrumb model={items} className='m-4' />
             <Card className="m-4 p-2">
-                <DataTable lazy totalRecords={customerData?.customerAdminList?.count ? customerData?.customerAdminList?.count : 0} onPage={(value) => console.log(value)} value={customerData?.customerAdminList?.customers ? customerData?.customerAdminList?.customers : []} paginator rows={1000} rowsPerPageOptions={[1000, 2000, 2500, 5000]}>
+                <DataTable 
+                    lazy 
+                    loading={loading}
+                    rows={pageData?.rows || 5}
+                    first={pageData?.first || 1} 
+                    totalRecords={customerData?.customerAdminList?.count ? customerData?.customerAdminList?.count : 0} onPage={(value) => setPageData(value)} 
+                    value={customerData?.customerAdminList?.customers ? customerData?.customerAdminList?.customers : []} paginator 
+                    rowsPerPageOptions={[5,10,20,50]}
+                >
                     <Column field="_id" header="ID"></Column>
                     <Column field="name" header="Name"></Column>
                     <Column field="email" header="Email"></Column>
