2b761033368a09c62995018a3f9a0e04a9f87309
mubin <mubin.ice.ru@gmail.com>
2024-07-26 02:18:59 +0600

    update uat server url

--- util/urlUtils.ts
+++ util/urlUtils.ts
@@ -1,5 +1,8 @@
-const env:any = 'production'
+// const env:any = 'production'
 // const env = 'dev'
-export const loginApiUrl = env === 'dev' ?"http://localhost:3001/graphql" : "http://kocart.com/api/graphql/";
-export const loginUrl = env === 'dev' ? "http://localhost:3000/api/auth/" : "http://kocart.com/api/auth/";
-export const baseUrl = env === 'dev' ? "http://localhost:3001/graphql" : "http://kocart.com/api/graphql/" 
+const env:any = 'uat'
+export const loginApiUrl = env === 'dev' ?"http://localhost:3001/graphql" : env === 'uat' ? "http://52.201.228.135/api/graphql/": "http://kocart.com/api/graphql/";
+
+export const loginUrl = env === 'dev' ? "http://localhost:3000/api/auth/" : env === 'uat' ? "http://52.201.228.135/api/auth/": "http://kocart.com/api/auth/";
+
+export const baseUrl = env === 'dev' ? "http://localhost:3001/graphql" : env === 'uat' ? "http://52.201.228.135/api/graphql/" : "http://kocart.com/api/graphql/"
