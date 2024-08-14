6cd0bc74bf238084404d459eb504d324bad9fe4f
mubin <mubin.ice.ru@gmail.com>
2024-08-15 02:53:39 +0600

    add stock update options

--- app/kocart/stock-management/page.tsx
+++ app/kocart/stock-management/page.tsx
@@ -2,17 +2,35 @@
 import { GET_STOCK_LIST } from "@/graphql/stock-management";
 import { useLazyQuery, useQuery } from "@apollo/client";
 import { BreadCrumb } from "primereact/breadcrumb";
+import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
+import { Dialog } from "primereact/dialog";
 import { Image } from "primereact/image";
+import { InputNumber } from "primereact/inputnumber";
+import { InputText } from "primereact/inputtext";
 import { useEffect, useState } from "react";
+import Select from 'react-select'
+
+const stockUpdateOptions = [
+    {
+        label:"Add",
+        value: "add"
+    },
+    {
+        label: "Subtract",
+        value: 'subtract'
+    }
+]
 
 export default function Brand() {
     const [pageData, setPageData] = useState<any>({rows:5,first:0})
     const [getStocks, {data, loading}] = useLazyQuery(GET_STOCK_LIST, {
         fetchPolicy:"no-cache"
     })
+    const [visible, setVisible] = useState(false);
+    const [item, setItem] = useState<any>({})
 
     useEffect(()=>{
         getStocks({variables: {
@@ -43,7 +61,17 @@ export default function Brand() {
         </p>
     )
 
-    const attributeActionRenderer =  (item:any)=>(<i className="pi pi-pen-to-square cursor-pointer"/>)
+    const attributeActionRenderer =  
+    (item:any)=>(
+        <i 
+            onClick={()=>{
+                setVisible(true);
+                setItem(item)
+                console.log(item)
+            }} 
+            className="pi pi-pen-to-square cursor-pointer"
+        />
+    )
 
     const stockVariantRenderer = (item:any)=>(
         <div>
@@ -97,6 +125,23 @@ export default function Brand() {
                     <Column header="Vaiants - Stock" body={stockVariantRenderer}/>
                 </DataTable>
             </Card>
+            <Dialog position="top" header="Manage Stock" visible={visible} style={{ width: '50vw' }} onHide={() => {if (!visible) return; setVisible(false); }}>
+                <p className="my-3 font-semibold">Product</p>
+                <InputText 
+                    className="w-12" 
+                    value={item?.productType === 'variable' ? item?.attributeValues?.map((d:any)=>d?.valueName)?.join('-'):item?.productType} 
+                    disabled
+                />
+                <div className="flex w-12 flex-column">
+                    <p className="my-3 font-semibold">Current Stock</p>
+                    <InputNumber className="w-12" value={item?.totalStock}/>
+                    <p className="my-3 font-semibold">Quantity <span style={{ color: "red" }}>*</span></p>
+                    <InputNumber className="w-12"/>
+                    <p className="my-3 font-semibold">Type</p>
+                    <Select className="w-12" options={stockUpdateOptions} defaultValue={stockUpdateOptions[0]}/>
+                </div>
+                <Button className="w-12 mt-4" label="Submit"/>
+            </Dialog>
         </div>
     )
 }
