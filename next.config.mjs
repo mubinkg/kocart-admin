ce055781ad5e68e696c84f9de3e94249d24a0568
mubinkg <mubin.ice.ru@gmail.com>
2024-10-21 21:39:19 +0600

    update port

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'https://kocart.com/:path*',
-          },
-        ]
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://kocart.com/:path*',
       },
-  };
+    ]
+  },
+};
 
 export default nextConfig;
