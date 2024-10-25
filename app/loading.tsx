9482b609a7246af80705e8b9770a91df6d75b2c5
mubinkg <mubin.ice.ru@gmail.com>
2024-10-25 21:29:18 +0600

    add locader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-export default function Loading(){
+export default function Loading() {
   return (
-    <div>
-      Loading..
-    </div>
+    <html lang="en">
+      <body className={`m-0 p-0`}>
+        <h1>Loading</h1>
+      </body>
+    </html>
   )
 }
