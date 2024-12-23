162237a69c5f1954a3b62ee7be8cc38bc429a676
mubinkg <mubin.ice.ru@gmail.com>
2024-12-23 22:30:20 +0600

    update the base url

--- util/urlUtils.ts
+++ util/urlUtils.ts
@@ -1,8 +1,8 @@
 const env:any = 'production'
 // const env = 'dev'
 
-export const loginApiUrl = env === 'dev' ? "http://localhost:3001/graphql" : "http://35.208.70.7/seller-signin";
+export const loginApiUrl = env === 'dev' ? "http://localhost:3001/graphql" : "http://35.208.48.138/seller-signin";
 
-export const loginUrl = env === 'dev' ? "http://localhost:3000/api/auth/" : "http://35.208.70.7/api/auth/";
+export const loginUrl = env === 'dev' ? "http://localhost:3000/api/auth/" : "http://35.208.48.138/api/auth/";
 
-export const baseUrl = env === 'dev' ? "http://localhost:3001/graphql" : "http://35.208.70.7/api/graphql/"
+export const baseUrl = env === 'dev' ? "http://localhost:3001/graphql" : "http://35.208.48.138/api/graphql/"
